05848cb3e370b662946b34dba8fb7d413dbaa19b
Pedro Leite <110786845+PedroLeiteDeMenezes@users.noreply.github.com>
2024-05-01 09:32:03 -0300

    commit

--- js.js
+++ js.js
@@ -4,7 +4,7 @@ containers.forEach(container => {
     container.addEventListener('mouseenter', () => {
         const imgSrc = container.querySelector('img').getAttribute('src');
         const apresentacao = document.querySelector('.apresentacao__2');
-        apresentacao.style.backgroundImage = `linear-gradient(rgba(15, 15, 15, 0.8), rgba(15, 15, 15, 0.9)), url(${imgSrc})`;
+        apresentacao.style.backgroundImage = `linear-gradient(rgba(15, 15, 15, 0.95), rgba(15, 15, 15, 0.9)), url(${imgSrc})`;
         apresentacao.style.backgroundRepeat = "no-repeat";
         apresentacao.style.backgroundSize = "cover";
     });
