622447e8e05aca48b72167e0ad437557c6f4a101
Pedro Leite <110786845+PedroLeiteDeMenezes@users.noreply.github.com>
2024-05-19 11:13:00 -0300

    finished project

--- js.js
+++ js.js
@@ -1,4 +1,5 @@
 const containers = document.querySelectorAll('.grid__all > div');
+const popular = document.querySelectorAll('.popular > div');
 
 containers.forEach(container => {
     container.addEventListener('mouseenter', () => {
@@ -9,3 +10,13 @@ containers.forEach(container => {
         apresentacao.style.backgroundSize = "cover";
     });
 });
+
+popular.forEach(popular =>{
+    popular.addEventListener('mouseenter', () => {
+        const imgSrc = popular.querySelector('img').getAttribute('src');
+        const apresentacao = document.querySelector('.apresentacao__2');
+        apresentacao.style.backgroundImage = `linear-gradient(rgba(15, 15, 15, 0.95), rgba(15, 15, 15, 0.9)), url(${imgSrc})`;
+        apresentacao.style.backgroundRepeat = "no-repeat";
+        apresentacao.style.backgroundSize = "cover"
+    }) 
+})
